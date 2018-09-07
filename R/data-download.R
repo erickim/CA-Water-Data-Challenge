8e97b77daa273476794b8f00210bbf330468e4e4
Eric Kim <ekim339@berkeley.edu>
2018-09-07 10:58:54 -0700

    editted data scraping

--- R/data-download.R
+++ R/data-download.R
@@ -8,112 +8,50 @@ library(tidyverse)
 
 gsheet_path <- "data/datasets-gsheets.xlsx"
 
-sheets <- excel_sheets(gsheet_path)
-sheets_dir <- gsub(" ", "-", sheets)
-
-docs <- lapply(sheets,
-               function(sheet) {
-                 read_excel(gsheet_path, sheet = sheet)
-                 })
-names(docs) <- sheets
-
-# if youre on pc this probably wont work
-for (i in sheets_dir) {
-  system(paste0("mkdir data/", i))
-}
+doc <- read_excel(gsheet_path, sheet = "Recommended")
 
 # I'm looking to use purrr's pluck here, not rvest's
-links <- lapply(sheets[1:2],
-                function(sheet) {
-                  docs %>%
-                    pluck(sheet) %>%
-                    filter(grepl("http", `Link to Open Data Format`)) %>%
-                    pull(`Link to Open Data Format`)
-                })
-names(links) <- sheets[1:2]
+links <- doc %>%
+  filter(grepl("http", `Link to Open Data Format`)) %>%
+  pull(`Link to Open Data Format`)
 
 download_data <- function(links) {
   links %>%
     read_html() %>%
     html_nodes("a") %>%
     html_attr("href") %>%
-    data.frame(links = .) %>%
-    filter(grepl(pattern = "pdf|csv|json", links)) %>%
-    pull(links) %>%
+    data.frame(link = .) %>%
+    filter(grepl(pattern = "pdf|csv|json", link)) %>%
+    pull(link) %>%
     as.character()
 }
 
 # downloading recommended links
-recommended_links <- links %>%
-  pluck(sheets[1]) %>%
+doc_links <- links %>%
   map(download_data)
-recommended_links[[17]] <- links %>% pluck(sheets[1]) %>% pluck(17)
-names(recommended_links) <- docs %>%
-  pluck(sheets[1]) %>%
+doc_links[[17]] <- links %>% pluck(17)
+names(doc_links) <- doc %>%
   filter(grepl("http", `Link to Open Data Format`)) %>%
   pull(`Data Set Name`) %>%
   gsub(pattern = "\\(|\\)|\\r\\n|-|\\*|,|:|\\/|\\'", replacement = "") %>%
   gsub(pattern = " ", replacement = "_")
 # no easily downloadable file for this one
-recommended_links$USGS_Groundwater_Levels <- NULL
+doc_links$USGS_Groundwater_Levels <- NULL
 
-for (i in names(recommended_links)) {
-  command <- paste0("mkdir data/", sheets_dir[1], "/", i)
+for (i in names(doc_links)) {
+  command <- paste0("mkdir data/", i)
   system(command)
-  
-  links <- recommended_links %>% pluck(i)
-  
-  # prevent overflowing of connection
-  Sys.sleep(2)
-  
-  for (j in 1:length(links)) {
-    start <- links[j] %>% regexpr(pattern = "\\/[^\\/]*$")
-    
-    file <- paste0("data/", sheets_dir[1], "/", i, "/",
-                   substring(links[j],
+
+  loop_links <- doc_links %>% pluck(i)
+
+  for (j in 1:length(loop_links)) {
+    start <- loop_links[j] %>% regexpr(pattern = "\\/[^\\/]*$")
+
+    file <- paste0("data/", i, "/",
+                   substring(loop_links[j],
                              first = start + 1,
-                             last = nchar(links[j])))
-    
-    download.file(url = links[j], destfile = file)
+                             last = nchar(loop_links[j])))
+
+    download.file(url = loop_links[j], destfile = file)
   }
 }
-
-
-# downloading wolfram candidates linkes
-wolfram_candidates_links <- links %>%
-  pluck(sheets[2]) %>%
-  map(download_data)
-names(wolfram_candidates_links) <- docs %>%
-  pluck(sheets[2]) %>%
-  filter(grepl("http", `Link to Open Data Format`)) %>%
-  pull(`Data Set Name`) %>%
-  gsub(pattern = "\\(|\\)|\\r\\n|-|\\*|,|:|\\/|\\'", replacement = "") %>%
-  gsub(pattern = " ", replacement = "_")
-
-for (i in names(recommended_links)) {
-  command <- paste0("mkdir data/", sheets_dir[2], "/", i)
-  system(command)
-  
-  links <- recommended_links %>% pluck(i)
-  
-  # prevent overflowing of connection
-  Sys.sleep(2)
-  
-  for (j in 1:length(links)) {
-    start <- links[j] %>% regexpr(pattern = "\\/[^\\/]*$")
-    
-    file <- paste0("data/", sheets_dir[2], "/", i, "/",
-                   substring(links[j],
-                             first = start + 1,
-                             last = nchar(links[j])))
-    
-    download.file(url = links[j], destfile = file)
-  }
-}
-
-
-# should get the following links
-# dict
-# https://data.ca.gov/sites/default/files/PUBLIC%20WATER%20SYSTEMS%20DATA%20DICTIONARY_0.pdf
-# data
-# https://data.ca.gov/sites/default/files/Public%20Potable%20Water%20Systems%20FINAL%2006-22-2018_0.csv
