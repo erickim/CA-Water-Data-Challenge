ca5d3f3c44056f306b58aacc9bf6f5cbf5bfdfdb
tao-feng <kev1n.ch3n@gmail.com>
2018-09-20 11:19:37 -0700

    add school data

---
+++ R/pubschools_Cleaning.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+public_schools <- read_tsv("data/pubschls.txt")
+
+public_schools_active <- public_schools %>%
+  filter(StatusType == "Active") %>%
+  mutate(missing = is.na(AdmEmail1) &
+           is.na(AdmEmail2) &
+           is.na(AdmEmail3))
+
+public_schools_active %>%
+  filter(missing) %>%
+  write_csv(path = "data/pubschls_active_missingcontacts.csv")
+
+public_schools_active %>%
+  filter(!missing) %>%
+  write_csv(path = "data/pubschls_active.csv")
